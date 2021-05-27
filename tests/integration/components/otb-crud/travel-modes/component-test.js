6b87bd9a1acae8b1d67d91d345a1dfa1d6f5fb36
Jay Varner <jayvarner@gmail.com>
2021-05-27 18:17:27 -0400

    Add modes test, fix CircleCI config

---
+++ tests/integration/components/otb-crud/travel-modes/component-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | otb-crud/travel-modes', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<OtbCrud::TravelModes />`);
+
+    assert.dom('div.uk-width-1-2').hasText('Mode');
+
+  });
+});

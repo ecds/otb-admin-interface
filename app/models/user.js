b959b4120179bb079b9c4b75cd2a6959c5a6e2e8
Jay Varner <jayvarner@gmail.com>
2021-07-14 14:40:26 -0400

    Imporve user management

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 import Model, { hasMany, attr } from '@ember-data/model';
+import { isEmpty } from '@ember/utils';
 
 export default class User extends Model {
   @attr('string') displayName;
@@ -10,4 +11,14 @@ export default class User extends Model {
   @hasMany('tour-set') tourSets;
   @hasMany('tour') tours;
   @hasMany('tour-author') tourAuthors;
+
+  get tourSetNames() {
+  if (this.super) {
+    return 'All';
+  }
+  else if (isEmpty(this.tourSets)) {
+      return "none";
+    }
+    return this.tourSets.map(tourSet => tourSet.name).join(', ');
+  }
 }

5951b73f629373d3f74a760f8e9ecb1a7f59f326
Jay Varner <jayvarner@gmail.com>
2021-09-23 11:20:02 -0400

    Move map icons to model

--- app/models/stop.js
+++ app/models/stop.js
@@ -1,4 +1,6 @@
 import Model, { belongsTo, hasMany, attr } from '@ember-data/model';
+import { icon as faIcon } from '@fortawesome/fontawesome-svg-core';
+/* global google */
 
 export default class Stop extends Model {
   @attr('string') title;
@@ -53,5 +55,45 @@ export default class Stop extends Model {
 
   get sortedMedia() {
     return this.stopMedia.sortBy('position');
+  }
+
+  get icon() {
+    // if (this.mapIcon.get('originalImageUrl')) {
+    //   return this.mapIcon.get('originalImageUrl');
+    // }
+    const icon = this.imageIcon ? this.imageIcon : this.markerIconSVG;
+    if (this.active) {
+      icon.scale = .125;
+      icon.scaledSize = new google.maps.Size(100, 100);
+    }
+    return icon;
+  }
+
+  get markerIconSVG() {
+    return {
+      fillColor: this.iconColor,
+      fillOpacity: 1,
+      scale: 0.075,
+      label: 'X',
+      labelOrigin: new google.maps.Point(200, 200),
+      anchor: new google.maps.Point(200, 600),
+      path: faIcon({ prefix: 'fas', iconName: 'map-marker' }).icon.lastObject
+    };
+  }
+
+  get imageIcon() {
+    const url = this.mapIcon.get('originalImageUrl');
+    return url ? { url } : null;
+  }
+
+  get parkingIconSVG() {
+    return {
+      path: faIcon({ prefix: 'fas', iconName: 'square' }).icon.lastObject,
+      fillColor: 'blue',
+      fillOpacity: 1,
+      scale: 0.075,
+      labelOrigin: new google.maps.Point(250, 250),
+      anchor: new google.maps.Point(200, 600)
+    };
   }
 }
